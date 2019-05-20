4ae4af246f1f0be6e9980094d0f4515dba37e7ad
yoko <yoko@transposit.com>
2019-05-20 20:51:32 +0000

    Commit from Transposit IDE

---
+++ src/insert_to_kinesis.js
@@ -0,0 +1,40 @@
+(params) => {
+  //let data = api.run("airtable.get_records", {baseId: params.baseId, table:'event_queue'}, {limit: 2})[0];
+  
+  let data = api.run("this.sqs_receive_message")[0].ReceiveMessageResponse.ReceiveMessageResult.Message;
+  if (!data || !data.ReceiptHandle) {
+    api.log("no data, going back to sleep");
+    return;
+  }
+  
+  const deleteHandle = data.ReceiptHandle;
+
+  let records = data.Body;
+  if (!records || records.length == 0) {
+    api.log("no data, going back to sleep");
+    return;
+  }
+  
+  try {
+    records = JSON.parse(records);
+  } catch {
+    api.log("Unable to parse, deleting: " + records);
+    return api.run("this.delete_message", {handle: deleteHandle});
+  }
+  
+
+  let result = api.run("aws_kinesis_firehose.put_record_batch", {
+    DeliveryStreamName: params.deliveryStreamName, 
+    Records: records});
+
+  console.log(deleteHandle)
+  if (result[0].FailedPutCount == 0) {
+    api.log(api.run("this.delete_message", {handle: deleteHandle}))
+  }
+  return result
+}
+
+/*
+ * For sample code and reference material, visit
+ * https://api-composition.transposit.com/references/js-operations
+ */
