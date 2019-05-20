e86b39211c8f9e102aec563f3bc6e4e536d05240
yoko <yoko@transposit.com>
2019-05-20 21:51:05 +0000

    Commit from Transposit IDE

--- src/queue_events.js
+++ src/queue_events.js
@@ -1,6 +1,6 @@
 (params) => {
-  
-	const _ = require('underscore.js');
+
+    const _ = require('underscore.js');
     const moment = require('moment-timezone-with-data.js');
     const startTime = moment().tz('America/Los_Angeles').startOf('day').subtract(parseInt(params.daysAgo), 'days');
     const endTime = startTime.clone().add(params.daysAgo, 'days');
@@ -17,24 +17,26 @@
     // custom logic for transforming events. Please modify this section so the logic works for your log format
     logObjects = _.compact(logObjects.map(e => {
         try {
-          return JSON.parse(e.message.split(" - ")[1]);
+            return JSON.parse(e.message.split(" - ")[1]);
         } catch (error) {
-          console.log(error.message)
-          return null
+            console.log(error.message)
+            return null
         }
     }));
-  
-  // put log objects to sqs in batch, and we can later take them from the queue for processing
-  for(let i = 0; i < logObjects.length; i+=params.batchSize) {
-    console.log(put_queue(logObjects.slice(i, i+params.batchSize)));
-  }
-  
-  
-  function put_queue(records) {
-    return api.run('aws_sqs.send_message', {MessageBody: JSON.stringify(records), 
-                                            QueueUrl : params.queueUrl})
-  }
-  
+
+    // put log objects to sqs in batch, and we can later take them from the queue for processing
+    for (let i = 0; i < logObjects.length; i += params.batchSize) {
+        console.log(put_queue(logObjects.slice(i, i + params.batchSize)));
+    }
+
+
+    function put_queue(records) {
+        return api.run('aws_sqs.send_message', {
+            MessageBody: JSON.stringify(records),
+            QueueUrl: params.queueUrl
+        })
+    }
+
 }
 
 /*
