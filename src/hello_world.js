f0169fe7138ae9f1a06b11471ca2bab531c6a5d6
yoko <yoko@transposit.com>
2019-05-20 20:45:54 +0000

    Commit from Transposit IDE

--- src/hello_world.js
+++ src/hello_world.js
@@ -1,9 +1,9 @@
 (params) => {
-  const params = {logGroupName: "/aws/ecs/prod/web",
+  let parameters = {logGroupName: "/aws/ecs/prod/web",
                filterPattern: params.filterPattern,
                startTime: params.startTime,
                endTime: params.endTime}
-  return api.run("aws_cloudwatch_logs.filter_log_events", params)
+  return api.run("aws_cloudwatch_logs.filter_log_events", parameters)
  
 }
 
