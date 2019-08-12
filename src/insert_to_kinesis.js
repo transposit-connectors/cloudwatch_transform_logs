7f149143b081b9582b8ab773f0cd43c8c5516be5
yoko <yoko@transposit.com>
2019-08-12 18:01:01 +0000

    Commit from Transposit IDE

--- src/insert_to_kinesis.js
+++ src/insert_to_kinesis.js
@@ -1,45 +1,56 @@
 (params) => {
-    // get a message off of a SQS queue 
-    let data = api.run("aws_sqs.receive_message",
-        { QueueUrl: params.queueUrl, MaxNumberOfMessages: 1 })[0].ReceiveMessageResponse.ReceiveMessageResult.Message;
+  // get a message off of a SQS queue
+  let data = api.run("aws_sqs.receive_message", {
+    $body: { QueueUrl: params.queueUrl, MaxNumberOfMessages: 1 }
+  })[0].ReceiveMessageResponse.ReceiveMessageResult.Message;
 
+  if (!data || !data.ReceiptHandle) {
+    api.log("no data, going back to sleep");
+    return;
+  }
 
-    if (!data || !data.ReceiptHandle) {
-        api.log("no data, going back to sleep");
-        return;
+  const deleteHandle = data.ReceiptHandle;
+
+  let records = data.Body;
+  if (!records || records.length == 0) {
+    api.log("no data, going back to sleep");
+    return;
+  }
+
+  try {
+    records = JSON.parse(records);
+  } catch {
+    api.log("Unable to parse, deleting: " + records);
+    return api.run("aws_sqs.delete_message", {
+      $body: {
+        ReceiptHandle: deleteHandle,
+        QueueUrl: params.queueUrl
+      }
+    });
+  }
+
+  // put events to Kinesis firehose stream https://aws.amazon.com/kinesis/data-firehose/
+  // you can customize the consumer of Kinesis firehose to be S3
+  let result = api.run("aws_kinesis_firehose.put_record_batch", {
+    $body: {
+      DeliveryStreamName: params.deliveryStreamName,
+      Records: records
     }
+  });
 
-    const deleteHandle = data.ReceiptHandle;
-
-    let records = data.Body;
-    if (!records || records.length == 0) {
-        api.log("no data, going back to sleep");
-        return;
-    }
-
-    try {
-        records = JSON.parse(records);
-    } catch {
-        api.log("Unable to parse, deleting: " + records);
-        return api.run("aws_sqs.delete_message", { ReceiptHandle: deleteHandle, QueueUrl: params.queueUrl });
-    }
-
-
-    // put events to Kinesis firehose stream https://aws.amazon.com/kinesis/data-firehose/
-    // you can customize the consumer of Kinesis firehose to be S3
-    let result = api.run("aws_kinesis_firehose.put_record_batch",
-        {
-            DeliveryStreamName: params.deliveryStreamName,
-            Records: records
-        });
-	
-    
-	// delete job from the queue
-    if (result[0].FailedPutCount == 0) {
-     	console.log("delete message handle = " + deleteHandle)
-        api.log(api.run("aws_sqs.delete_message", { ReceiptHandle: deleteHandle, QueueUrl: params.queueUrl }))
-    }
-    return result
+  // delete job from the queue
+  if (result[0].FailedPutCount == 0) {
+    console.log("delete message handle = " + deleteHandle);
+    api.log(
+      api.run("aws_sqs.delete_message", {
+        $body: {
+          ReceiptHandle: deleteHandle,
+          QueueUrl: params.queueUrl
+        }
+      })
+    );
+  }
+  return result;
 }
 
 /*
